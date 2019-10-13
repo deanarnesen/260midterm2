62f988523788fad9ad715b6ffd70bc86ff4d7c19
Mark Clement <mjcleme@gmail.com>
2019-10-13 04:33:52 +0000

    working with vue

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,87 @@
+/*global axios */
+/*global Vue */
+var app = new Vue({
+  el: '#voting',
+  data: {
+    candidates: [],
+    candidate: '',
+    ballot: [],
+  },
+  created: function() {
+    this.getall();
+  },
+  methods: {
+    addCandidate() {
+      var url = "http://class.clementbyu.com:8080/voting";
+      axios.post(url, {
+          Name: this.candidate,
+          votes: 0
+        })
+        .then(response => {
+          console.log("Post Response ");
+          console.log(response.data);
+          this.candidates.push(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+      console.log(this.candidates);
+      this.candidate = "";
+    },
+    deleteCandidate(candidate) {
+      var index = this.candidates.indexOf(candidate);
+      if (index > -1) {
+        var url = "http://class.clementbyu.com:8080/voting/" + candidate._id;
+        axios.delete(url)
+          .then(response => {
+            console.log(response.data.votes);
+            this.getall();
+          })
+          .catch(e => {
+            console.log(e);
+          });
+        console.log("URL " + url);
+      }
+    },
+    upvote(candidate) {
+      var url = "http://class.clementbyu.com:8080/voting/" + candidate._id + "/upvote";
+      console.log("upvote URL " + url);
+      axios.put(url)
+        .then(response => {
+          console.log(response.data.votes);
+          candidate.votes = response.data.votes;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    async getall() {
+      console.log("get all");
+      var url = "http://class.clementbyu.com:8080/voting"; // This is the route we set up in index.js
+      try {
+        let response = await axios.get(url);
+        this.candidates = response.data; // Assign array to returned response
+        console.log(this.candidates);
+        return true;
+      }
+      catch (error) {
+        console.log(error);
+      }
+    },
+    dovote() {
+      console.log("In Dovote");
+      for(var candidate of this.candidates) {
+        if (candidate.selected) {
+          this.upvote(candidate);
+          this.ballot.push(candidate);
+        }
+      }
+    },
+  }
+});
+
+
+/*
 angular.module('voting',[])
 .controller('MainCtrl',[
   '$scope','$http',
@@ -53,3 +137,4 @@ angular.module('voting',[])
     };
   }
 ]);
+*/
